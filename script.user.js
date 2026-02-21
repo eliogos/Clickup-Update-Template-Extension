a2db624a1bce63edfcb058e4a02cc34c3bb8996b
eliogos <einsturd@gmail.com>
2026-02-22 03:25:13 +0800

    fix(ui): keep dark half-keys and improve border clarity

--- script.user.js
+++ script.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         ClickUp --update Modal
 // @namespace    clickup-update-modal
-// @version      15.0.78
+// @version      15.0.79
 // @description  Insert update template in ClickUp
 // @match        https://app.clickup.com/*
 // @grant        GM_info
