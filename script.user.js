72d31b1bd69095962c9b035f047027caf2fc2a1c
eliogos <einsturd@gmail.com>
2026-02-22 03:29:58 +0800

    fix(ui): adjust party half-keys for light mode

--- script.user.js
+++ script.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         ClickUp --update Modal
 // @namespace    clickup-update-modal
-// @version      15.0.82
+// @version      15.0.83
 // @description  Insert update template in ClickUp
 // @match        https://app.clickup.com/*
 // @grant        GM_info
